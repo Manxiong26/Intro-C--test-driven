7537a4c2d4f91c106bff10bb7b4fd8d0b3123d02
Maneena Xiong <xiong.maneena@gmail.com>
2022-06-03 20:57:45 -0500

    changed console to integratedTerminal from internalConsole, also added more line of code for response in Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,11 @@
             var name = Console.ReadLine();
             var currentDate = DateTime.Now;
             Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.Write($"{Environment.NewLine}What can I help you with {name}?");
+            var response = Console.ReadLine();
+            Console.WriteLine($"{Environment.NewLine}You want to me to {response}?");
+            var responseTwo = Console.ReadLine();
+            Console.WriteLine($"{Environment.NewLine}I'll {response} then! Give me one minute");
             Console.ReadKey(true);
         }
     }
